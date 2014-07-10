609049fab463e0b7d6d2a2b315e0f58172333b89
Tim de Koning <git@king-management.nl>
2014-07-10 14:27:19 +0200

    Fixed array processing

--- src/JsonSchemaForm/DataParser.php
+++ src/JsonSchemaForm/DataParser.php
@@ -20,7 +20,21 @@ class DataParser {
 					case 'boolean':
 						$parsedValue = (boolean) $value;
 						break;
+
+					case 'number':
+						$parsedValue = (int) $value;
+						break;
+
+					case 'array':
+						$parsedValue = array();
+						foreach ($value as $item) {
+							$parsedValue[] = DataParser::parse($item, $schemaNode->items);
+						}
+						break;
+
 					default:
+						echo 'Datatype not supported by DataParser: ' . $schemaNode->type . PHP_EOL;
+						print_r($value);
 						print_r($schemaNode);
 						exit;
 				}
