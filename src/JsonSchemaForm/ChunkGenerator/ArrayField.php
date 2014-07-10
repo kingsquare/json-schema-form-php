a87c454a925df283803d5718b730ff8dd15cc707
Tim de Koning <git@king-management.nl>
2014-07-10 12:20:11 +0200

    Better array support

--- src/JsonSchemaForm/ChunkGenerator/ArrayField.php
+++ src/JsonSchemaForm/ChunkGenerator/ArrayField.php
@@ -1,15 +1,30 @@
 <?php
-
 namespace JsonSchemaForm\ChunkGenerator;
 
 class ArrayField extends \JsonSchemaForm\ChunkGenerator {
 	public function render(array $options) {
 		$subFormGenerator = new \JsonSchemaForm\Generator($this->schema->items);
-		$subFormGeneratorOptions = $options;
-		$subFormGeneratorOptions['name'] = 'item 0';
-		$subFormGeneratorOptions['isSubForm'] = true;
-		$options['content'] = $subFormGenerator->render($subFormGeneratorOptions);
+
+		if (!empty($options['value'])) {
+			$options['content'] = '';
+			foreach ($options['value'] as $key => $item) {
+				$options['content'] .= $subFormGenerator->render(
+					$this->getSubFormGeneratorOptions($options, $key),
+					$item
+				);
+			}
+		} else {
+			// At least show a single items (maybe as a template for other items?)
+			$options['content'] = $subFormGenerator->render($this->getSubFormGeneratorOptions($options, 0));
+		}
 		return $this->_render('chunk/array.twig', $options);
+	}
+
+	private function getSubFormGeneratorOptions($options, $key) {
+		$subFormGeneratorOptions = $options;
+		$subFormGeneratorOptions['name'] = 'item ' . $key;
+		$subFormGeneratorOptions['isSubForm'] = true;
+		return $subFormGeneratorOptions;
 	}
 
 }
