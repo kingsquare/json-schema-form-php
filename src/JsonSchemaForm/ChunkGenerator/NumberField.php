29b1704f6f5fe4181dfd1335728c5419066b6b1e
Tim de Koning <git@king-management.nl>
2014-07-17 11:51:02 +0200

    enums for numbers

--- src/JsonSchemaForm/ChunkGenerator/NumberField.php
+++ src/JsonSchemaForm/ChunkGenerator/NumberField.php
@@ -4,7 +4,24 @@ namespace JsonSchemaForm\ChunkGenerator;
 
 class NumberField extends \JsonSchemaForm\ChunkGenerator {
 	public function render($options = array()) {
-		$inputType = (isset($this->schema->inputType) ? $this->schema->inputType : 'number');
+		if (empty($this->schema->enum)) {
+			$inputType = (isset($this->schema->inputType) ? $this->schema->inputType : 'number');
+			return $this->_render('chunk/' . $inputType . '.twig', $options);
+		}
+
+		$options['options'] = array();
+		foreach ($this->schema->enum as $enumValue) {
+			$enumTitles = (isset($this->schema->enumTitles) ? (object) $this->schema->enumTitles : new StdClass());
+			$enumTitleKey = (string) $enumValue;
+
+			$options['options'][] = array(
+				'id' =>  $this->getDomCompatible(implode(array_merge($this->path, array($enumValue)),  '-')),
+				'label' => (isset($enumTitles->$enumTitleKey) ? $enumTitles->$enumTitleKey: $enumValue),
+				'value' => $enumValue
+			);
+		}
+
+		$inputType = (isset($this->schema->inputType) ? $this->schema->inputType : 'select');
 		return $this->_render('chunk/' . $inputType . '.twig', $options);
 	}
 }
